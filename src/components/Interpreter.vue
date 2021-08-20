7982e576a44c73a92fb8374a4185e10c99b3b6e0
Kresh195 <KreshF5@mail.ru>
2021-08-20 16:55:35 +0700

    Initial commit

---
+++ src/components/Interpreter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="interpreter">
+    <h3>Interpreter:</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.interpreter {
+  background: aqua;
+}
+
+</style>
